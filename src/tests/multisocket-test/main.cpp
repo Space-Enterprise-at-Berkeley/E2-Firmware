42092ce7e68a284c9893adc35731da57d7a08d17
Andy Cate <and164v@gmail.com>
2023-03-11 14:37:08 -0800

    test code

---
+++ src/tests/multisocket-test/main.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+
+#include "Common.h"
+#include "EspComms.h"
+
+uint32_t task_example() { 
+  
+  Serial.println("Hello World!");
+  Comms::emitPacketToGS();
+  Comms::emitPacketToAll();
+  return 1000 * 1000;
+  
+}
+
+Task taskTable[] = {
+  {task_example, 0, true},
+};
+
+#define TASK_COUNT (sizeof(taskTable) / sizeof (struct Task))
+
+void setup() {
+  Serial.begin(921600);
+  // setup stuff here
+  Comms::init(); // takes care of Serial.begin()
+
+  while(1) {
+    // main loop here to avoid arduino overhead
+    for(uint32_t i = 0; i < TASK_COUNT; i++) { // for each task, execute if next time >= current time
+      uint32_t ticks = micros(); // current time in microseconds
+      if (taskTable[i].nexttime - ticks > UINT32_MAX / 2 && taskTable[i].enabled) {
+        taskTable[i].nexttime = ticks + taskTable[i].taskCall();
+      }
+    }
+    Comms::processWaitingPackets();
+  }
+}
+
+void loop() {}
