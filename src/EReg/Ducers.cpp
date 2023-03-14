69b804434b460a392aa7406feca914c06fbe0872
Sekhar Musunuri <sekhar.musunuri@berkeley.edu>
2023-03-14 11:12:13 -0700

    edge case pt stuff

--- src/EReg/Ducers.cpp
+++ src/EReg/Ducers.cpp
@@ -98,6 +98,9 @@ namespace Ducers {
      * @return *float* which ducer to use
     */
    float chooseDucerRead(float ducer1, float ducer2) {
+        if ((ducer1 < -50) && (ducer2 < -50)) {
+            return 10000; //easy way to abort
+        }
        return max(ducer1, ducer2);
    }
 
