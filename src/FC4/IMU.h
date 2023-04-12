fe63d5a80d7a3778cf78c3d92ee329845eff4d3c
Andy Cate <and164v@gmail.com>
2023-04-12 00:46:21 -0700

    everything in progress nothing is done

--- src/FC4/IMU.h
+++ src/FC4/IMU.h
@@ -3,20 +3,30 @@
 #include "HAL.h"
 #include <EspComms.h>
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM6DSO32.h>
+#include <SparkFun_KX13X.h> 
 
 namespace IMU {
+
+    Adafruit_LSM6DSO32 imu;
+    SparkFun_KX134 imu_highg;
+    
     extern uint32_t imuUpdatePeriod;
 
-    extern float qW;
-    extern float qX;
-    extern float qY;
-    extern float qZ;
-    extern float accelX;
-    extern float accelY;
-    extern float accelZ;
+    sensors_event_t accel;
+    sensors_event_t gyro;
+    sensors_event_t temp;
+    
+    extern float accelX_high;
+    extern float accelY_high;
+    extern float accelZ_high;
 
     void initIMU();
     uint32_t imuSample();
+
+    void initIMUHighG();
+    uint32_t imuHighGSample();
 
     float get_acceleration_x();
     float get_acceleration_y();
@@ -25,5 +35,9 @@ namespace IMU {
     float get_gyro_x();
     float get_gyro_y();
     float get_gyro_z();
+
+    float get_accel_x_highg();
+    float get_accel_y_highg();
+    float get_accel_z_highg();
 
 };
