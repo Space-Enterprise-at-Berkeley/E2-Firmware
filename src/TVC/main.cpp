1d9dfdcd782693a4cbc09830ef29632300d779ed
Sekhar Musunuri <sekhar.musunuri@berkeley.edu>
2023-09-23 17:05:02 -0700

    update packetdefs

--- src/TVC/main.cpp
+++ src/TVC/main.cpp
@@ -44,11 +44,11 @@ void setup() {
   HAL::resetEncoders();
   TVC::init();
   Serial.printf("setup other stuff!\n");
-  Comms::registerCallback(2, TVC::enableCircle);
-  Comms::registerCallback(3, zeroTVC);
-  Comms::registerCallback(4, TVC::stopTVC);
-  Comms::registerCallback(5, TVC::setRadius);
-  Comms::registerCallback(6, TVC::setAngle);
+  Comms::registerCallback(102, TVC::enableCircle);
+  Comms::registerCallback(103, zeroTVC);
+  Comms::registerCallback(104, TVC::stopTVC);
+  Comms::registerCallback(105, TVC::setRadius);
+  Comms::registerCallback(106, TVC::setAngle);
   // Comms::registerCallback(5, TVC::setTVCMode);
   // Comms::registerCallback(101, joystickCommand);
   // Comms::registerCallback(2, TVC::printEncoders);
