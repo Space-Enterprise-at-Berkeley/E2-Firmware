eff67f704216e7f6b60a374ef2015f13fa39bf37
samhitag3 <samhita.ghosh@berkeley.edu>
2023-10-23 22:41:30 -0700

    temp

--- src/RS485_Receiver/main.cpp
+++ src/RS485_Receiver/main.cpp
@@ -5,41 +5,102 @@
 
 #define TASK_COUNT (sizeof(taskTable) / sizeof(struct Task))
 
-#define TEST_SERIAL Serial1 // TODO: verify this
-#define E_REG_TEST_TRANSMIT 19
-#define E_REG_TEST_RECEIVE 18
+#define LOX_SERIAL Serial4 // TODO: verify this
+#define LOX_REN_PIN 19
+#define FUEL_SERIAL Serial3 // TODO: verify this
+#define FUEL_REN_PIN 18
 
 char loxBuffer[sizeof(Comms::Packet)];
 int loxCnt = 0;
+char fuelBuffer[sizeof(Comms::Packet)];
+int fuelCnt = 0;
 
 void setup() {
     // hardware setup
     Serial.begin(115200);
-    TEST_SERIAL.begin(115200);
+    LOX_SERIAL.begin(115200);
+    FUEL_SERIAL.begin(115200);
 
     Comms::initComms();
-
-    Serial.println("SETUP");
 
     DEBUG("STARTING UP\n");
     DEBUG_FLUSH();
 
-    pinMode(E_REG_TEST_TRANSMIT, OUTPUT);
-    pinMode(E_REG_TEST_RECEIVE, OUTPUT);
+    pinMode(LOX_REN_PIN, OUTPUT);
+    pinMode(FUEL_REN_PIN, OUTPUT);
 
-    digitalWriteFast(E_REG_TEST_TRANSMIT, LOW);
-    digitalWriteFast(E_REG_TEST_RECEIVE, LOW);
-    delay(200);
+    digitalWriteFast(LOX_REN_PIN, LOW);
+    digitalWriteFast(FUEL_REN_PIN, LOW);
 }
 
 void loop() {
-        Serial.println("loop");
-        while(TEST_SERIAL.available() && loxCnt < 256) {
-            Serial.println("serial available");
-            TEST_SERIAL.write(loxBuffer, loxCnt);
-            loxBuffer[loxCnt] = TEST_SERIAL.read();
+        // if (LOX_SERIAL.available()) {
+        //     Serial.println("hello world");
+        // }
+
+        // while(LOX_SERIAL.available()) {
+        //     loxBuffer[loxCnt] = LOX_SERIAL.read();
+        //     loxCnt++;
+        // }
+        // Serial.write(loxBuffer, loxCnt);
+        // loxCnt = 0;
+
+        // while(FUEL_SERIAL.available()) {
+        //     fuelBuffer[fuelCnt] = FUEL_SERIAL.read();
+        //     fuelCnt++;
+        // }
+        // Serial.write(fuelBuffer, fuelCnt);
+        // fuelCnt = 0;
+
+        while(FUEL_SERIAL.available() && fuelCnt < 256) {
+            fuelBuffer[fuelCnt] = FUEL_SERIAL.read();
+            if(fuelCnt == 0 && ((uint8_t)fuelBuffer[0] != 21 && (uint8_t) fuelBuffer[0] != 22)) {
+                Serial.print((uint8_t)fuelBuffer[0]);
+                Serial.print(" ");
+                continue;
+            }
+            if(fuelBuffer[fuelCnt] == '\n') {
+                Serial.println("got newline");
+                Comms::Packet *packet = (Comms::Packet *)&fuelBuffer;
+                if(Comms::verifyPacket(packet)) {
+                    fuelCnt = 0;
+                    Serial.print("fuel: ");
+                    Serial.print(packet->id);
+                    Serial.print(" : ");
+                    Serial.print(Comms::packetGetFloat(packet, 0));
+                    Serial.print("\n");
+                    Comms::emitPacket(packet);
+                    break;
+                }
+            }
+            fuelCnt++;
+        }
+        if(fuelCnt == 256) {
+            Serial.println("RESETTING FUEL BUFFER\n");
+            fuelCnt = 0;
+        }
+
+
+        while(LOX_SERIAL.available() && loxCnt < 256) {
+            loxBuffer[loxCnt] = LOX_SERIAL.read();
+            if(loxCnt == 0 && ((uint8_t)loxBuffer[0] != 21 && (uint8_t) loxBuffer[0] != 22)) {
+                Serial.print((uint8_t)loxBuffer[0]);
+                Serial.print(" ");
+                continue;
+            }
             if(loxBuffer[loxCnt] == '\n') {
                 Serial.println("got newline");
+                Comms::Packet *packet = (Comms::Packet *)&loxBuffer;
+                if(Comms::verifyPacket(packet)) {
+                    loxCnt = 0;
+                    Serial.print("lox: ");
+                    Serial.print(packet->id);
+                    Serial.print(" : ");
+                    Serial.print(Comms::packetGetFloat(packet, 0));
+                    Serial.print("\n");
+                    Comms::emitPacket(packet);
+                    break;
+                }
             }
             loxCnt++;
         }
@@ -47,5 +108,56 @@ void loop() {
             Serial.println("RESETTING LOX BUFFER\n");
             loxCnt = 0;
         }
-        delay(200);
+
     }
+// #include <Common.h>
+// #include <TeensyComms.h>
+
+// #include <Arduino.h>
+
+// #define TASK_COUNT (sizeof(taskTable) / sizeof(struct Task))
+
+// #define TEST_SERIAL Serial1 // TODO: verify this
+// #define E_REG_TEST_TRANSMIT 19
+// #define E_REG_TEST_RECEIVE 18
+
+// char loxBuffer[sizeof(Comms::Packet)];
+// int loxCnt = 0;
+
+// void setup() {
+//     // hardware setup
+//     Serial.begin(115200);
+//     TEST_SERIAL.begin(115200);
+
+//     Comms::initComms();
+
+//     Serial.println("SETUP");
+
+//     DEBUG("STARTING UP\n");
+//     DEBUG_FLUSH();
+
+//     pinMode(E_REG_TEST_TRANSMIT, OUTPUT);
+//     pinMode(E_REG_TEST_RECEIVE, OUTPUT);
+
+//     digitalWriteFast(E_REG_TEST_TRANSMIT, LOW);
+//     digitalWriteFast(E_REG_TEST_RECEIVE, LOW);
+//     delay(200);
+// }
+
+// void loop() {
+//         Serial.println("loop");
+//         while(TEST_SERIAL.available() && loxCnt < 256) {
+//             Serial.println("serial available");
+//             TEST_SERIAL.write(loxBuffer, loxCnt);
+//             loxBuffer[loxCnt] = TEST_SERIAL.read();
+//             if(loxBuffer[loxCnt] == '\n') {
+//                 Serial.println("got newline");
+//             }
+//             loxCnt++;
+//         }
+//         if(loxCnt == 256) {
+//             Serial.println("RESETTING LOX BUFFER\n");
+//             loxCnt = 0;
+//         }
+//         delay(200);
+//     }
