17d100ecc2d98641dec25f90303a0297f0d3c227
samhitag3 <samhita.ghosh@berkeley.edu>
2023-10-25 00:57:29 -0700

    receiving works

--- src/RS485_EReg_Test/main.cpp
+++ src/RS485_EReg_Test/main.cpp
@@ -1,6 +1,12 @@
 
 #include <Arduino.h>
 
+#include <Wire.h>
+#include <FDC2214.h>
+#include <TMP236.h>
+
+#include <CircularBuffer.h>
+
 #include <EspComms.h>
 #include <Common.h>
 
@@ -10,46 +16,59 @@ int indicatorDuty = 500;
 int indicatorPeriod = 1000;
 int indicatorLastTime = 0;
 
-unsigned long previousMillis = 0;
+
+const int timeBetweenTransmission = 100; // ms
+int lastTransmissionTime = 0;
+
 const long interval = 25;
 
-const uint8_t logSecs = 5;
-
-#define TASK_COUNT (sizeof(taskTable) / sizeof (struct Task))
-
-void setup() {
-  // setup stuff here
-  // Comms::init(); // takes care of Serial.begin()
+void setup()
+{
   Serial.begin(115200);
-  Serial.print("HELLO");
   Serial1.begin(115200);
   Serial1.setPins(17, 18);
-  Serial.print("HELLO");
-
-  // while(1) {
-  //   // CAP START
-  //   unsigned long currentMillis = millis();
-  //   if (currentMillis - previousMillis >= interval) {
-  //     previousMillis = currentMillis;
-  //     Serial.println("HELLOOOO WORLD");
-  //   }
-  //   int timeNow = currentMillis = millis();
-  //   if(timeNow - indicatorLastTime >= indicatorPeriod) {
-  //     indicatorLastTime = timeNow;
-  //   }
-  //   delay(200);
-  // }
 }
 
-void loop() {
+unsigned long previousMillis = 0;
+
+Comms::Packet eregPacket = {.id = 23};
+
+void loop()
+{
+
+  if(millis() - lastTransmissionTime >= timeBetweenTransmission) {
+    DEBUG("Transmitting ");
+    DEBUG(Comms::packetGetFloat(&eregPacket, 0));
+    DEBUG("\n");
+    DEBUG_FLUSH();
+    lastTransmissionTime = lastTransmissionTime + timeBetweenTransmission;
+    Serial1.write(eregPacket.id);
+    Serial1.write(eregPacket.len);
+    Serial1.write(eregPacket.timestamp, 4);
+    Serial1.write(eregPacket.checksum, 2);
+    Serial1.write(eregPacket.data, eregPacket.len);
+    Serial1.write('\n');
+    Serial1.flush();
+  }
+
   unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= interval) {
-      previousMillis = currentMillis;
-      Serial.println("eregging");
-    }
-    int timeNow = currentMillis = millis();
-    if(timeNow - indicatorLastTime >= indicatorPeriod) {
-      indicatorLastTime = timeNow;
-    }
-    delay(200);
+
+  if (currentMillis - previousMillis >= interval)
+  {
+    eregPacket.len = 0;
+    Comms::packetAddFloat(&eregPacket, 4.2069);
+    
+    uint32_t timestamp = millis();
+    eregPacket.timestamp[0] = timestamp & 0xFF;
+    eregPacket.timestamp[1] = (timestamp >> 8) & 0xFF;
+    eregPacket.timestamp[2] = (timestamp >> 16) & 0xFF;
+    eregPacket.timestamp[3] = (timestamp >> 24) & 0xFF;
+
+    uint16_t checksum = Comms::computePacketChecksum(&eregPacket);
+    eregPacket.checksum[0] = checksum & 0xFF;
+    eregPacket.checksum[1] = checksum >> 8;
+
+    Serial.println("done!");
+  }
+  delay(200);
 }
--- src/RS485_Receiver/eregmain.cpp
+++ src/RS485_Receiver/eregmain.cpp
@@ -6,7 +6,6 @@
 #define TASK_COUNT (sizeof(taskTable) / sizeof(struct Task))
 
 #define TEST_SERIAL Serial3 // TODO: verify this
-// #define TRANSMIT_PIN 19
 #define RECEIVE_PIN 18
 
 char buffer[sizeof(Comms::Packet)];
@@ -15,91 +14,42 @@ int cnt = 0;
 void setup() {
     // hardware setup
     Serial.begin(115200);
-    Serial.println("SETUP");
     TEST_SERIAL.begin(115200);
 
     Comms::initComms();
-
-    // pinMode(TRANSMIT_PIN, OUTPUT);
     pinMode(RECEIVE_PIN, OUTPUT);
 
-    // digitalWriteFast(TRANSMIT_PIN, HIGH);
     digitalWriteFast(RECEIVE_PIN, LOW);
 }
 
 void loop() {
-    Serial.println("MAIN LOOP");
-    while(TEST_SERIAL.available() && cnt < 256) {
-        Serial.println("ITS AVAILABLE!!");
-        buffer[cnt] = TEST_SERIAL.read();
-        if(cnt == 0 && ((uint8_t)buffer[0] != 21 && (uint8_t) buffer[0] != 22)) {
-            Serial.print((uint8_t)buffer[0]);
-            Serial.print(" ");
-            continue;
-        }
-        if(buffer[cnt] == '\n') {
-            Serial.println("got newline");
-            Comms::Packet *packet = (Comms::Packet *)&buffer;
-            if(Comms::verifyPacket(packet)) {
-                cnt = 0;
-                Serial.print("fuel: ");
-                Serial.print(packet->id);
-                Serial.print(" : ");
-                Serial.print(Comms::packetGetFloat(packet, 0));
-                Serial.print("\n");
-                Comms::emitPacket(packet);
-                break;
+        while(TEST_SERIAL.available() && cnt < 256) {
+            Serial.println("AVAILABLEEEE");
+            buffer[cnt] = TEST_SERIAL.read();
+            if(cnt == 0 && ((uint8_t)buffer[0] != 23)) {
+                Serial.print((uint8_t)buffer[0]);
+                Serial.print(" ");
+                continue;
             }
+            if(buffer[cnt] == '\n') {
+                Serial.println("got newline");
+                Comms::Packet *packet = (Comms::Packet *)&buffer;
+                if(Comms::verifyPacket(packet)) {
+                    cnt = 0;
+                    Serial.print("EREG: ");
+                    Serial.print(packet->id);
+                    Serial.print(" : ");
+                    Serial.print(Comms::packetGetFloat(packet, 0));
+                    Serial.print("\n");
+                    Comms::emitPacket(packet);
+                    break;
+                }
+            }
+            cnt++;
         }
-        cnt++;
+        if(cnt == 256) {
+            Serial.println("RESETTING BUFFER\n");
+            cnt = 0;
+        }
+        delay(200);
     }
-    if(cnt == 256) {
-        Serial.println("RESETTING BUFFER\n");
-        cnt = 0;
-    }
-    delay(200);
-}
-
-// #define E_REG_TEST_TRANSMIT 19
-// #define E_REG_TEST_RECEIVE 18
-
-// char loxBuffer[sizeof(Comms::Packet)];
-// int loxCnt = 0;
-
-// void setup() {
-//     // hardware setup
-//     Serial.begin(115200);
-//     TEST_SERIAL.begin(115200);
-
-//     Comms::initComms();
-
-//     Serial.println("SETUP");
-
-//     DEBUG("STARTING UP\n");
-//     DEBUG_FLUSH();
-
-//     pinMode(E_REG_TEST_TRANSMIT, OUTPUT);
-//     pinMode(E_REG_TEST_RECEIVE, OUTPUT);
-
-//     digitalWriteFast(E_REG_TEST_TRANSMIT, LOW);
-//     digitalWriteFast(E_REG_TEST_RECEIVE, LOW);
-//     delay(200);
-// }
-
-// void loop() {
-//         Serial.println("loop");
-//         while(TEST_SERIAL.available() && loxCnt < 256) {
-//             Serial.println("serial available");
-//             TEST_SERIAL.write(loxBuffer, loxCnt);
-//             loxBuffer[loxCnt] = TEST_SERIAL.read();
-//             if(loxBuffer[loxCnt] == '\n') {
-//                 Serial.println("got newline");
-//             }
-//             loxCnt++;
-//         }
-//         if(loxCnt == 256) {
-//             Serial.println("RESETTING LOX BUFFER\n");
-//             loxCnt = 0;
-//         }
-//         delay(200);
-//     }
