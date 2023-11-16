5bdcdec88f7467fe2dbf3785ebbfdc6541fdcbd0
samhitag3 <samhita.ghosh@berkeley.edu>
2023-11-16 04:42:36 -0800

    basic gps set up, switch to i2c?

--- src/E2FC/HAL.h
+++ src/E2FC/HAL.h
@@ -2,6 +2,8 @@
 
 #include <Common.h>
 
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
+
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
@@ -10,6 +12,8 @@ namespace HAL {
 
     const uint8_t RS485_RECEIVE = 36;
     const uint8_t RS485_TRANSMIT = 37;
+
+    const uint8_t gpsCSPin = 38;
 
     void initHAL();
 }
