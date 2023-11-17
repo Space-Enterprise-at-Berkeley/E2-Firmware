109d5bfdf6ea22d2994e3b76efd88747290a690c
Nihal Singh Gulati <nihalsgulati@gmail.com>
2023-11-16 23:51:44 -0800

    capfill reciever code works

--- src/RS485_Teensy/main.cpp
+++ src/RS485_Teensy/main.cpp
@@ -8,9 +8,6 @@
 #define FUEL_SERIAL Serial3 // TODO: verify this
 #define FUEL_REN_PIN 18
 
-char fuelBuffer[sizeof(Comms::Packet)];
-int fuelCnt = 0;
-
 void setup() {
     // hardware setup
     Serial.begin(115200);
@@ -23,32 +20,26 @@ void setup() {
 }
 
 void loop() {
-        while(FUEL_SERIAL.available() && fuelCnt < 256) {
-            fuelBuffer[fuelCnt] = FUEL_SERIAL.read();
-            if(fuelCnt == 0 && ((uint8_t)fuelBuffer[0] != 21 && (uint8_t) fuelBuffer[0] != 22)) {
-                Serial.print((uint8_t)fuelBuffer[0]);
-                Serial.print(" ");
-                continue;
-            }
-            if(fuelBuffer[fuelCnt] == '\n') {
-                Serial.println("got newline");
-                Comms::Packet *packet = (Comms::Packet *)&fuelBuffer;
-                if(Comms::verifyPacket(packet)) {
-                    fuelCnt = 0;
-                    Serial.print("fuel: ");
-                    Serial.print(packet->id);
-                    Serial.print(" : ");
-                    Serial.print(Comms::packetGetFloat(packet, 0));
-                    Serial.print("\n");
-                    Comms::emitPacket(packet);
-                    break;
-                }
-            }
-            fuelCnt++;
-        }
-        if(fuelCnt == 256) {
-            Serial.println("RESETTING FUEL BUFFER\n");
-            fuelCnt = 0;
-        }
-        delay(200);
+    char fuelBuffer[sizeof(Comms::Packet)];
+    int fuelCnt = 0;
+    while(FUEL_SERIAL.available()) {
+        fuelBuffer[fuelCnt] = FUEL_SERIAL.read();
+        fuelCnt++;
     }
+    if(fuelCnt > 0) {
+        // process
+        Comms::Packet *packet = (Comms::Packet *)&fuelBuffer;
+        if(Comms::verifyPacket(packet)) {
+            Serial.print("fuel: ");
+            Serial.print(packet->id);
+            Serial.print(" : ");
+            Serial.print(Comms::packetGetFloat(packet, 0));
+            Serial.print("\n");
+        } else {
+            Serial.print("bad");
+        }
+        Comms::emitPacket(packet);
+        fuelCnt=0;
+    }
+    delay(100);
+}
