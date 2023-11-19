e3763e5bc20538b5a38ecd47f11dfe45afa14d92
samhitag3 <samhita.ghosh@berkeley.edu>
2023-11-19 08:18:43 -0800

    dumb stuff

--- src/E2FC/HAL.h
+++ src/E2FC/HAL.h
@@ -15,8 +15,8 @@
 #include <Wire.h>
 namespace HAL { 
     // Serial
-    #define RS485_SERIAL1 Serial1;
-    #define RS485_SERIAL0 Serial0;
+    #define RS485_SERIAL1 Serial1
+    #define RS485_SERIAL0 Serial0
 
     // Breakouts
     extern ADS8167 ads8167; // ADC, SPI
