e69784bc72b6a49459795eadc7898371526f0e25
zanderdenning <zrdenning@gmail.com>
2024-01-28 00:52:30 -0800

    RS422 test stuff

--- src/tests/rs422ereg-test/main.cpp
+++ src/tests/rs422ereg-test/main.cpp
@@ -6,85 +6,11 @@
 #include <Wire.h>
 #include <SPI.h>
 
-
-// 0: Ground, 1: Flight
-uint8_t vehicleState = 0; 
-
-uint32_t helloWorld() {
-    Serial.println("yo");
-    Comms::sendFirmwareVersionPacket(Comms::Packet(), 0);
-    return 1000 * 1000;
-}
-
-char oregbuffer[sizeof(Comms::Packet)];
-uint8_t ocnt = 0;
-char fregbuffer[sizeof(Comms::Packet)];
-uint8_t fcnt = 0;
-uint32_t sampleOReg() {
-    while(OREG_SERIAL.available()) {
-        Serial.println("o available");
-        uint8_t c = OREG_SERIAL.read();
-        oregbuffer[ocnt] = c;
-        Serial.println(ocnt);
-        Serial.println(c);
-        if(oregbuffer[ocnt] == '\n') {
-            Comms::Packet *packet = (Comms::Packet *)&oregbuffer;
-            Serial.println(packet->id);
-            Serial.println(packet->len);
-            if(Comms::verifyPacket(packet)) {
-                DEBUG("Found oreg packet with ID ");
-                DEBUG(packet->id);
-                DEBUG('\n');
-                if (packet->id == 133) {
-                  Comms::sendAbort(packet->data[0], packet->data[1]);
-                }
-                else if (packet->id <= 10) {
-                  packet->id += 30;
-                  Comms::emitPacketToGS(packet);
-                }
-                else if (packet->id == 102) {
-                  packet->id = 42;
-                  Comms::emitPacketToGS(packet);
-                }
-                ocnt = 0;
-                //Comms::emitPacket(packet, &RADIO_SERIAL, "\r\n\n", 3);
-                break;
-            }
-        }
-        ocnt++;
-    }
-    while(FREG_SERIAL.available()) {
-        Serial.println("f available");
-        fregbuffer[fcnt] = FREG_SERIAL.read();
-        if(fregbuffer[fcnt] == '\n') {
-            Comms::Packet *packet = (Comms::Packet *)&fregbuffer;
-            if(Comms::verifyPacket(packet)) {
-                DEBUG("Found freg packet with ID ");
-                DEBUG(packet->id);
-                DEBUG('\n');
-                if (packet->id == 133) {
-                  Comms::sendAbort(packet->data[0], packet->data[1]);
-                }
-                else if (packet->id <= 10) {
-                  packet->id += 50;
-                  Comms::emitPacketToGS(packet);
-                }
-                else if (packet->id == 102) {
-                  packet->id = 62;
-                  Comms::emitPacketToGS(packet);
-                }
-                fcnt = 0;
-                //Comms::emitPacket(packet, &RADIO_SERIAL, "\r\n\n", 3);
-                break;
-            }
-        }
-    }
-    return 40 * 1000;
-}
-
 void sendToOreg(Comms::Packet packet, int8_t offset) {
   Serial.println("Oreg send");
-  OREG_SERIAL.write(packet.id + offset);
+  packet.id += offset;
+  Comms::finishPacket(&packet);
+  OREG_SERIAL.write(packet.id);
   OREG_SERIAL.write(packet.len);
   OREG_SERIAL.write(packet.timestamp, 4);
   OREG_SERIAL.write(packet.checksum, 2);
@@ -94,12 +20,101 @@ void sendToOreg(Comms::Packet packet, int8_t offset) {
 
 void sendToFreg(Comms::Packet packet, int8_t offset) {
   Serial.println("Freg send");
-  FREG_SERIAL.write(packet.id + offset);
+  packet.id += offset;
+  Comms::finishPacket(&packet);
+  FREG_SERIAL.write(packet.id);
   FREG_SERIAL.write(packet.len);
   FREG_SERIAL.write(packet.timestamp, 4);
   FREG_SERIAL.write(packet.checksum, 2);
   FREG_SERIAL.write(packet.data, packet.len);
   FREG_SERIAL.write('\n');
+}
+
+// 0: Ground, 1: Flight
+uint8_t vehicleState = 0; 
+
+uint32_t helloWorld() {
+    Comms::Packet packet = {.id = 'E', .len = 0};
+    Comms::finishPacket(&packet);
+    sendToOreg(packet, 0);
+    sendToFreg(packet, 0);
+    return 1000 * 1000;
+}
+
+char oregbuffer[sizeof(Comms::Packet)];
+uint8_t ocnt = 0;
+char fregbuffer[sizeof(Comms::Packet)];
+uint8_t fcnt = 0;
+uint32_t sampleOReg() {
+    while(OREG_SERIAL.available()) {
+        // Serial.println("o available");
+        uint8_t c = OREG_SERIAL.read();
+        oregbuffer[ocnt] = c;
+        if(oregbuffer[ocnt] == '\n') {
+            Comms::Packet *packet = (Comms::Packet *)&oregbuffer;
+            Serial.println("Got oreg packet");
+            if(Comms::verifyPacket(packet)) {
+                Serial.print("Found oreg packet with ID ");
+                Serial.print(packet->id);
+                Serial.print('\n');
+                if (packet->id == 133) {
+                  Comms::sendAbort(packet->data[0], packet->data[1]);
+                }
+                else if (packet->id <= 10) {
+                  packet->id += 30;
+                  Comms::finishPacket(packet);
+                  Comms::emitPacketToGS(packet);
+                }
+                else if (packet->id == 102) {
+                  packet->id = 42;
+                  Comms::finishPacket(packet);
+                  Comms::emitPacketToGS(packet);
+                }
+                //Comms::emitPacket(packet, &RADIO_SERIAL, "\r\n\n", 3);
+            }
+            ocnt = 0;
+            break;
+        }
+        ocnt++;
+        if (ocnt > sizeof(Comms::Packet)) {
+          ocnt = 0;
+        }
+    }
+    while(FREG_SERIAL.available()) {
+        // Serial.println("f available");
+        uint8_t c = FREG_SERIAL.read();
+        fregbuffer[fcnt] = c;
+        if(fregbuffer[fcnt] == '\n') {
+            Comms::Packet *packet = (Comms::Packet *)&fregbuffer;
+            Serial.println("Got freg packet");
+            if(Comms::verifyPacket(packet)) {
+                Serial.print("Found freg packet with ID ");
+                Serial.print(packet->id);
+                Serial.print('\n');
+                if (packet->id == 133) {
+                  Comms::sendAbort(packet->data[0], packet->data[1]);
+                }
+                else if (packet->id <= 10) {
+                  packet->id += 50;
+                  Comms::finishPacket(packet);
+                  Comms::emitPacketToGS(packet);
+                }
+                else if (packet->id == 102) {
+                  packet->id = 62;
+                  Comms::finishPacket(packet);
+                  Comms::emitPacketToGS(packet);
+                }
+                //Comms::emitPacket(packet, &RADIO_SERIAL, "\r\n\n", 3);
+            }
+            fcnt = 0;
+            break;
+        }
+        fcnt++;
+        if (fcnt > sizeof(Comms::Packet)) {
+          fcnt = 0;
+        }
+    }
+    return 10;
 }
 
 Task taskTable[] = {
