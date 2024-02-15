0e2129fe0c6a08a7b2cc996360ace0d0cd36f769
C.T. Chen <ctychen@berkeley.edu>
2024-02-14 23:38:04 -0800

    automation changes by bavin/claire + add rs422 testscript

--- src/tests/serial-spam-test/main.cpp
+++ src/tests/serial-spam-test/main.cpp
@@ -8,15 +8,16 @@ char fregbuffer[sizeof(Comms::Packet)];
 uint8_t fcnt = 0;
 
 void setup() {
-    Serial.begin(921600);
+  Serial.begin(921600);
   // setup stuff here
-  Serial1.begin(115200, SERIAL_8N1, 38, 39);
-  Serial2.begin(115200, SERIAL_8N1, 36, 37);
+  Serial1.begin(921600, SERIAL_8N1, 38, 39); //oreg
+  Serial2.begin(921600, SERIAL_8N1, 36, 37); //freg
+  // FREG_SERIAL.begin(921600, SERIAL_8N1, rx1P, tx1P)
 
   while (1) {
 
-    while (Serial1.available()) {
-        Serial.print()
+    while (Serial2.available()) {
+        Serial.print(Serial2.read());
     }
 
   }
